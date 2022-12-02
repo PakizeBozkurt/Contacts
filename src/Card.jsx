4ae7574334f2cb1275611b2276db02b2471e82ec
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-02 22:48:19 +0000

    Created contacts

---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+      <h2>Pare</h2>
+      <img
+        src="https://images.unsplash.com/photo-1600294808794-57bcf94a0c9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHNrZWxldG9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        alt="avatar_img"
+      />
+      <p>+0123 456 789</p>
+        <p>p@pare.com</p>
+    </div>
+  );
+}
+
+export default Card
