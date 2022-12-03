4fab3ab2e21f45fac168be43b177078a7be899c7
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-03 00:50:42 +0000

    Completed contacts

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import Card from "./Card";
 function App() {
   return (
     <div>
-    <Card />
-    <Card />
-    <Card />
+      <h1>My Contacts</h1>
+      <Card
+        name="Pare"
+        img="https://images.unsplash.com/photo-1600294808794-57bcf94a0c9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHNrZWxldG9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        tel="+0123 456 789"
+        email="p@pare.com"
+      />
+      <Card
+        name="Bare"
+        img="https://images.unsplash.com/photo-1603252945711-7b44a2e2bb69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNrZWxldG9uJTIwYXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        tel="+9876 543 210"
+        email="b@bare.com"
+      />
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function Card() {
+function Card(props) {
   return (
-    <div>
-      <h2>Pare</h2>
-      <img
-        src="https://images.unsplash.com/photo-1600294808794-57bcf94a0c9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHNrZWxldG9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    <div className='my-style'>
+      <h2>{props.name}</h2>
+      <img className='img'
+        src={props.img}
         alt="avatar_img"
       />
-      <p>+0123 456 789</p>
-        <p>p@pare.com</p>
+      <p>{props.tel}</p>
+        <p>{props.email}</p>
     </div>
   );
 }
