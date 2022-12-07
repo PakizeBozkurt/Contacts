dcb0d81d22811faf6f30eb968db1e613bdd3b287
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-07 10:22:56 +0000

    Styling card

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
-  <h2 className="name">{props.name}</h2>
-        <img
-          className="circle-img"
-          src= {props.img}
-          alt="avatar_img"
-        />
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src={props.img} alt="avatar_img" />
       </div>
       <div className="bottom">
         <p className="info">{props.tel}</p>
--- src/contacs.js
+++ src/contacs.js
@@ -1,21 +1,19 @@
 const contacts = [
-    {
-        name: "Pare",
-        imgUrl: 
-        "https://images.unsplash.com/photo-1600294808794-57bcf94a0c9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHNrZWxldG9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-        phone: "+123 456 789",
-        email: "p@pare.com"
-    },
+  {
+    name: "Pare",
+    imgUrl:
+      "https://images.unsplash.com/photo-1600294808794-57bcf94a0c9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHNrZWxldG9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    phone: "+123 456 789",
+    email: "p@pare.com",
+  },
 
-    {
-        name: "Bare",
-        imgUrl: 
-       "https://images.unsplash.com/photo-1603252945711-7b44a2e2bb69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNrZWxldG9uJTIwYXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-        phone: "+9876 543 210",
-        email: "b@bare.com"
-    }
-]
+  {
+    name: "Bare",
+    imgUrl:
+      "https://images.unsplash.com/photo-1603252945711-7b44a2e2bb69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNrZWxldG9uJTIwYXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    phone: "+9876 543 210",
+    email: "b@bare.com",
+  },
+];
 
 export default contacts;
-
-
