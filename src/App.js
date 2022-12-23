5aa3732a1398468d7273553376a778d49d276291
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-23 22:38:42 +0000

    My avatar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "./components/Card";
 //import "/home/codeyourfuture/Documents/React/Projects/My Contacts/contacts/src/index.css";
-import contacts from "./contacs";
+import contacts from "./contacts";
 import Avatar from "./components/Avatar";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Avatar />
+      <Avatar img="https://images.unsplash.com/photo-1530210124550-912dc1381cb8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2tlbGV0b24lMjBhdmF0YXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
       <Card
         name={contacts[0].name}
         img={contacts[0].imgUrl}
