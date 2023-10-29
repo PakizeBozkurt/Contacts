9cf4e8031ab07bb535fde35844a8b5565771d802
Pakize Bozkurt <pakiozihak@gmail.com>
2023-10-29 23:31:00 +0000

    formated

--- server/connection.js
+++ server/connection.js
@@ -1,18 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 async function connect() {
   try {
     await mongoose.connect(
       "mongodb+srv://pakiozihak:hGJ7Q468awzxwEOC@hr.jdtf367.mongodb.net/?retryWrites=true&w=majority",
       {
-        //mongodb+srv://pakiozihak:hGJ7Q468awzxwEOC@hr.jdtf367.mongodb.net/?retryWrites=true&w=majority
-
         useNewUrlParser: true,
         useUnifiedTopology: true,
       }
     );
-    console.log('Successfully connected to MongoDB.');
+    console.log("Successfully connected to MongoDB.");
   } catch (err) {
-    console.error('Error connecting to MongoDB: ', err);
+    console.error("Error connecting to MongoDB: ", err);
   }
 }
 module.exports = connect;
